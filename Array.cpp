bd9e9f5d6501ead4733555f33e3c9913a39c7d21
wifilofi <90645177+wifilofi@users.noreply.github.com>
2025-12-11 21:49:22 +0300

    add base fields, functions

---
+++ Array.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by gfn on 12/11/2025.
+//
+
+#include "Array.h"
---
+++ Array.h
@@ -0,0 +1,51 @@
+﻿//
+// Created by gfn on 12/11/2025.
+//
+
+#ifndef CPP_LAB2_ARRAY_H
+#define CPP_LAB2_ARRAY_H
+
+template<typename T>
+class Array
+{
+private:
+    int capacity_ = 8;
+    int size_ = 0;
+
+public:
+    int size() const { return size_; }
+    int capacity() const { return capacity_; }
+
+    // inset at end
+    int insert(const T &value)
+    {
+    }
+
+    // insert by index
+    int insert(int index, const T &value)
+    {
+    }
+
+    // remove by index
+    void remove(int index)
+    {
+    }
+
+    class Iterator
+    {
+    };
+
+    class ConstIterator
+    {
+    };
+
+    Iterator iterator() { return Iterator(*this, 0); }
+    ConstIterator const_iterator() { return ConstIterator(*this, 0); }
+
+    Iterator reverseIterator() { return Iterator(*this, -1); }
+    ConstIterator reverseIterator() const { return ConstIterator(*this, -1); }
+
+};
+
+
+#endif //CPP_LAB2_ARRAY_H
